e7e0dd1d0d36331c8fe41a616e13bc95571c6517
aryawardanna <aryamadridfc@gmail.com>
2021-11-24 13:08:02 +0800

    update again

--- app/player/controller.js
+++ app/player/controller.js
@@ -25,18 +25,22 @@ module.exports = {
 
   detailPage: async (req, res) => {
     try {
-      const { id } = req.params;
-      const voucher = await Voucher.findOne({ _id: id })
+      const voucher = await Voucher.findOne({ _id: req.params.id })
+        .populate('user', '_id name phoneNumber')
         .populate('category')
-        .populate('nominals')
-        .populate('user', '_id name phoneNumber');
+        .populate('nominals');
 
       const payment = await Payment.find().populate('banks');
 
       if (!voucher) {
         return res.status(404).json({ message: 'voucher game not found.!' });
       }
-      res.status(200).json({ data: voucher, payment });
+      res.status(200).json({
+        data: {
+          detail: voucher,
+          payment,
+        },
+      });
     } catch (err) {
       res.status(500).json({ message: err.message || `Internal server error` });
     }
